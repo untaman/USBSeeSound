e7fb3d6d98079d994efa6328c9c2f0b084787ffd
untaman <97481499+untaman@users.noreply.github.com>
2022-01-12 14:27:18 -0500

    Update MainActivity.java

--- src/main/java/com/bodekjan/usb/MainActivity.java
+++ src/main/java/com/bodekjan/usb/MainActivity.java
@@ -147,6 +147,8 @@ public class MainActivity extends AppCompatActivity implements NoCopySpan {
         registerReceiver(broadcastReceiver, filter);
         hl= new Heavylifting();
         textView.setText("p: to get temp data hit a few time until you get some numbers\ns: sound buffer data \n +: increase sound threshold by 5 \n -: decrease sound threshold by 5\n");
+        
+        //get location when textView changes which corresponds the event of receiveing from USB which is directly related to a sound event if clear
         textView.addTextChangedListener(new TextWatcher() {
 
             public void afterTextChanged(Editable s) {
